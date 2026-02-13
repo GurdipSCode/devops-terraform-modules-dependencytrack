9375dc4a2a1ba49edb8efa928a278029b693ae77
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2026-02-13 01:47:00 +0000

    Add dependencytrack_project resource configuration

--- src/projects.tf
+++ src/projects.tf
@@ -1,1 +1,12 @@
+resource "dependencytrack_project" "this" {
+  for_each = var.projects
 
+  name        = each.value.name
+  description = each.value.description
+  version     = each.value.version
+  classifier  = each.value.classifier
+  active      = each.value.active
+  parent      = each.value.parent
+  tags        = each.value.tags
+  group       = each.value.group
+}
